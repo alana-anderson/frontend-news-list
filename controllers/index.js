cd3e8afd99531d5c67afdc32432af9256fe34aa1
alana anderson <alanatreimanis@gmail.com>
2016-01-26 17:29:57 -0500

    load articles in

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,5 @@ var list = require('./data');
 var articleList = list.articles;
 
 exports.index = function (req, res) {
-  res.render('', {list: articleList});
+  res.render('layout', {list: articleList});
 };
