92b258bd98ba9f3e3d3cc7e2151bc56477ab8d0a
alana anderson <alanatreimanis@gmail.com>
2016-01-28 10:45:06 -0500

    styling to table app

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,7 @@
 				cache: false,
 				success: function(data) {
 					$(data.data).each(function(i,a){
-						$('#articles').append('<tr><td><div class="col-md-1"><a href="'+a.url+'"><img src="'+a.image+' " height="50" width="50"/></a></div><div class="col-md-11"><h4><a href="'+a.url+'">'+ a.title+'</a></h4></div></td><td>'+a.profile.first_name+' '+a.profile.last_name+'</td><td>'+a.words+'</td><td>'+a.publish_at+'</td></tr>');
+						$('#articles').append('<tr><td><div class="col-md-1"><a href="'+a.url+'"><img src="'+a.image+' " height="50" width="50"/></a></div><div class="col-md-11"><h4><a href="'+a.url+'">'+ a.title+'</a></h4></div></td><td class="special-align align-left">'+a.profile.first_name+' '+a.profile.last_name+'</td><td class="special-align">'+a.words+'</td><td class="special-align">'+a.publish_at+'</td></tr>');
 					});
 					s = s+10;
 					i = i+10;
